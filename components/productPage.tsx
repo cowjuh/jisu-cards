1e96c25bf950670afecfec6bfadbbc47ced7247e
Jenny Zhang <jenzhang530@gmail.com>
2023-09-18 02:12:56 -0700

    Add product name

--- components/productPage.tsx
+++ components/productPage.tsx
@@ -7,6 +7,7 @@ type ProductDisplayProps = {
 const ProductDisplay: React.FC<ProductDisplayProps> = ({ product }) => {
   return (
     <div className="flex flex-col gap-4">
+      <h1 className="text-4xl">{product.name}</h1>
       <img src={product.image} alt={product.name} className="w-full h-auto mb-4" />
       <p className="mb-2">{product.description}</p>
       <hr className="border-t border-black h-0.5 mb-2" />
