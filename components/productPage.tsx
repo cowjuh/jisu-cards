743384d3f433b8137695eb96803b1ab700ba289d
Jenny Zhang <jenzhang530@gmail.com>
2023-09-18 10:46:46 -0700

    Display newlines

--- components/productPage.tsx
+++ components/productPage.tsx
@@ -9,11 +9,11 @@ const ProductDisplay: React.FC<ProductDisplayProps> = ({ product }) => {
   return (
     <div className="flex flex-col gap-4">
       <ProductCard product={product} />
-      <p className="mb-2">{product.description}</p>
+      <p className="mb-2 whitespace-pre-line">{product.description}</p>
       <hr className="border-t border-black h-0.5 mb-2" />
-      <p className="mb-2">{product.materials}</p>
+      <p className="mb-2 whitespace-pre-line">{product.materials}</p>
       <hr className="border-t border-black h-0.5 mb-2" />
-      <p className="mb-2">{product.additionalDetails}</p>
+      <p className="mb-2 whitespace-pre-line">{product.additionalDetails}</p>
       <hr className="border-t border-black h-0.5 mb-2" />
       <p className="text-2xl">${product.price}</p>
     </div>
