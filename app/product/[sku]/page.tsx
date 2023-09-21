1e9f69ff65ed388d6c891316ce186d211adad1b8
Jenny Zhang <jenzhang530@gmail.com>
2023-09-21 15:06:14 -0700

    Change product routing

--- app/product/[sku]/page.tsx
+++ app/product/[sku]/page.tsx
@@ -2,7 +2,7 @@ import ProductDisplay from "@/components/productPage";
 import { PRODUCTS } from "@/utils/constants";
 import Link from "next/link";
 
-export default function Page({ params }: { params: { sku: string; category: string } }) {
+export default function Page({ params }: { params: { sku: string } }) {
   const product = PRODUCTS.find((p) => p.sku === params.sku);
 
   // If product with the given SKU is not found, return a message or handle the case accordingly.
@@ -12,8 +12,8 @@ export default function Page({ params }: { params: { sku: string; category: stri
 
   return (
     <div>
-      <Link href={`/${params.category}`}>
-        <button>{`<- All ${params.category}`}</button>
+      <Link href={`/${product.type}`}>
+        <button>{`<- All ${product.type}`}</button>
       </Link>
       <ProductDisplay product={product} />
     </div>
