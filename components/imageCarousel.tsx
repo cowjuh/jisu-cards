58eb9035e9a78fa2af64d80511244279f0fa5896
Jenny Zhang <jenzhang530@gmail.com>
2023-09-21 19:44:41 -0700

    Add image modal

--- components/imageCarousel.tsx
+++ components/imageCarousel.tsx
@@ -2,30 +2,35 @@
 
 import React from "react";
 import Image, { StaticImageData } from "next/image";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 type ImageCarouselProps = {
   images: StaticImageData[];
 };
 
 const ImageCarousel = ({ images }: ImageCarouselProps) => {
-  console.log("Image Carousel");
-
-  const handleClick = () => {
-    console.log("clicked");
-  };
   return (
     <div>
-      <div className="flex overflow-x-scroll gap-4" onClick={handleClick}>
+      <div className="flex overflow-x-scroll gap-4">
         {images.map((img, i) => (
-          <div className="flex-none h-60 w-52 relative max-w-screen-sm sm:h-60 md:h-72 lg:h-80" key={i}>
-            <Image
-              src={img}
-              alt={`Supporting product image ${i + 1}`}
-              layout="fill"
-              objectFit="cover"
-              className="rounded"
-            />
-          </div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <div className="flex-none h-60 w-52 relative max-w-screen-sm sm:h-60 md:h-72 lg:h-80" key={i}>
+                <Image
+                  src={img}
+                  alt={`Supporting product image ${i + 1}`}
+                  layout="fill"
+                  objectFit="cover"
+                  className="rounded"
+                />
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <div className="p-4 w-full flex h-full" key={i}>
+                <Image src={img} alt={`Supporting product image ${i + 1}`} className="rounded" />
+              </div>
+            </DialogContent>
+          </Dialog>
         ))}
       </div>
     </div>
