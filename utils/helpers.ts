bc45f0ea0fa1958a368d6ff079e991628390f2bb
Jenny Zhang <jenzhang530@gmail.com>
2023-09-21 17:47:17 -0700

    Clean up code

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,6 +2,5 @@ import { PRODUCTS, ProductTypeEnum } from "./constants";
 
 export function getFirstProduct(category: ProductTypeEnum) {
   const products = PRODUCTS.filter((product) => product.type === category);
-  console.log(products[0]);
   return products[0];
 }
