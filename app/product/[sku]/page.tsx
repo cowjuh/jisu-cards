1d246fbac1d11d262ce789ae59d6ce56ea9b2bed
Jenny Zhang <jenzhang530@gmail.com>
2023-09-22 12:05:24 -0700

    Redirect capitalized SKU values in URL params

--- app/product/[sku]/page.tsx
+++ app/product/[sku]/page.tsx
@@ -1,22 +1,47 @@
+"use client";
+
 import ProductDisplay from "@/components/productPage";
-import { PRODUCTS } from "@/utils/constants";
+import { PRODUCTS, Product } from "@/utils/constants";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Page = ({ params }: { params: { sku: string } }) => {
-  const product = PRODUCTS.find((p) => p.sku.toLowerCase() === params.sku);
+  const [product, setProduct] = useState<Product | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(true); // Add loading state
+  const router = useRouter();
 
-  // If product with the given SKU is not found, return a message or handle the case accordingly.
-  if (!product) {
-    return <div>Product not found</div>;
-  }
+  useEffect(() => {
+    setLoading(true); // Set loading to true when starting to fetch
+    if (params.sku !== params.sku.toLowerCase()) {
+      console.log("rerouting to", params.sku.toLowerCase());
+      router.push(`/product/${params.sku.toLowerCase()}`);
+      return;
+    }
+    const product = PRODUCTS.find((p) => p.sku.toLowerCase() === params.sku);
+    if (!product) {
+      router.push(`/404`);
+    } else {
+      setProduct(product);
+    }
+    setLoading(false); // Set loading to false when done
+  }, []);
 
   return (
-    <div>
-      <Link href={`/${product.type}`}>
-        <button>{`<- All ${product.type}`}</button>
-      </Link>
-      <ProductDisplay product={product} />
-    </div>
+    <>
+      {loading ? (
+        <div>Loading...</div> // Show loading message while fetching
+      ) : (
+        product && (
+          <div>
+            <Link href={`/${product.type}`}>
+              <button>{`<- All ${product.type}`}</button>
+            </Link>
+            <ProductDisplay product={product} />
+          </div>
+        )
+      )}
+    </>
   );
 };
 
