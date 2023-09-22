1a63ac692d4237f63da2328b2ca260993b9037ba
Jenny Zhang <jenzhang530@gmail.com>
2023-09-21 17:47:58 -0700

    Remove dev code

--- components/imageCarousel.tsx
+++ components/imageCarousel.tsx
@@ -8,21 +8,8 @@ type ImageCarouselProps = {
 };
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
-  const [currentImg, setCurrentImg] = useState<StaticImageData>(images[0]);
-  const [open, setOpen] = useState<boolean>(false);
-
-  const handleSetCurrentImg = (img: StaticImageData) => () => {
-    setCurrentImg(img);
-  };
-
-  const handleClick = () => {
-    console.log("pls");
-    alert("pls");
-  };
-
   return (
     <div className="flex overflow-x-scroll gap-4">
-      <button onClick={handleClick}>hey</button>
       {images.map((img, i) => (
         <div className="flex-none h-60 w-52 relative max-w-screen-sm sm:h-60 md:h-72 lg:h-80" key={i}>
           <Image
